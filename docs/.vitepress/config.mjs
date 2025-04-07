28dcea74cdcac60c07ddf6e28137827de39d6386
lucifer <3616266449@qq.com>
2025-04-07 09:12:44 +0800

    add_ico

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,12 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "9lucifer",
   description: "A VitePress Site",
+  head: [
+    // 基础favicon
+    ['link', { rel: 'icon', href: '/favicon.ico', type: 'image/x-icon' }],
+    // 现代浏览器适配
+    ['link', { rel: 'icon', href: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' }],
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
