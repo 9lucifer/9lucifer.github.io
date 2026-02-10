5c4b6ff25af1d7cad03f757389cfa7938ec969d0
dinghaoran <haoran.ding@maxjia.com>
2026-02-10 21:23:37 +0800

    feat:测试xmind组件

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,10 @@
 import DefaultTheme from 'vitepress/theme'
-import XMindViewer from '../components/XMindViewer.vue'
+import XMindViewer from './../components/XMindViewer.vue'
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
+    app.component('x-mind-viewer', XMindViewer)
     app.component('XMindViewer', XMindViewer)
   }
 }
