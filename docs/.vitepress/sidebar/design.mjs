0b0eb0bed3f6f25c2717612191aa480ef21f72aa
dinghaoran <haoran.ding@maxjia.com>
2026-02-17 23:29:14 +0800

    fix:add 1亿玩家实时战绩排名方案

--- docs/.vitepress/sidebar/design.mjs
+++ docs/.vitepress/sidebar/design.mjs
@@ -3,7 +3,7 @@ export default [
     text: '系统设计',
     items: [
       { text: '权限管理鉴权模型', link: '/design/role-permission' },
-        {text :'1亿玩家实时战绩排名方案',link'/design/realtime-ranking-for-100m-players'},
+        {text :'1亿玩家实时战绩排名方案',link:'/design/realtime-ranking-for-100m-players'},
     ]
   }
 ];
