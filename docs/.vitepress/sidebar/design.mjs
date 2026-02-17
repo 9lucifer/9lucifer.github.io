e57abc65a0c231ac0b23e28c8933197621f2d022
dinghaoran <haoran.ding@maxjia.com>
2026-02-17 23:28:09 +0800

    feat:add 1亿玩家实时战绩排名方案

--- docs/.vitepress/sidebar/design.mjs
+++ docs/.vitepress/sidebar/design.mjs
@@ -2,7 +2,8 @@ export default [
   {
     text: '系统设计',
     items: [
-      { text: '权限管理鉴权模型', link: '/design/role-permission' }
+      { text: '权限管理鉴权模型', link: '/design/role-permission' },
+        {text :'1亿玩家实时战绩排名方案',link'/design/realtime-ranking-for-100m-players'},
     ]
   }
 ];
--- docs/design/index.md
+++ docs/design/index.md
@@ -5,3 +5,4 @@
 ## 学习记录
 
 - [权限管理鉴权模型](role-permission.md) - 权限管理鉴权模型
+- [1亿玩家实时战绩排名方案](realtime-ranking-for-100m-players.md) - 1亿玩家实时战绩排名方案
