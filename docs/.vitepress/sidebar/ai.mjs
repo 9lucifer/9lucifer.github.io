b7551ebd045e6a5689feea5a57de7c8457517bbd
dinghaoran <haoran.ding@maxjia.com>
2026-02-21 23:14:22 +0800

    fix:add AI名词汇总

--- docs/.vitepress/sidebar/ai.mjs
+++ docs/.vitepress/sidebar/ai.mjs
@@ -3,7 +3,9 @@ export default [
     text: 'AI 相关内容',
     items: [
       { text: 'AI 首页', link: '/ai/' },
-      { text: 'openclaw使用体验', link: '/ai/openclaw-after-user' }
+        { text: 'AI名词汇总', link: '/ai/ai-names-info' },
+        { text: 'openclaw使用体验', link: '/ai/openclaw-after-user' },
+
     ]
   }
 ];
--- docs/ai/index.md
+++ docs/ai/index.md
@@ -3,3 +3,4 @@
 ## 文章列表
 
 - [openclaw使用体验](openclaw-after-user.md)
+- [AI名词汇总](ai-names-info.md)
