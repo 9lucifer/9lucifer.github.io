f535fa46f9d8dfd80c627aecda1e3e49ce59c028
dinghaoran <haoran.ding@maxjia.com>
2026-02-25 13:18:58 +0800

    feat：fix add password

--- docs/.vitepress/sidebar/design.mjs
+++ docs/.vitepress/sidebar/design.mjs
@@ -4,7 +4,7 @@ export default [
     items: [
       { text: '权限管理鉴权模型', link: '/design/role-permission' },
       { text: '1亿玩家实时战绩排名方案', link: '/design/realtime-ranking-for-100m-players' },
-      { text: '*面试查缺补漏', link: '/i-interview-protected.html' }
+      { text: '*面试查缺补漏(已锁定，需密码)', link: '/i-interview-protected.html' }
     ]
   }
 ];
