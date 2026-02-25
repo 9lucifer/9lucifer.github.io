bf846be7681d320285b91da53f51b564eae2c112
dinghaoran <haoran.ding@maxjia.com>
2026-02-25 10:58:43 +0800

    feat：add openclaw-info

--- docs/.vitepress/sidebar/ai.mjs
+++ docs/.vitepress/sidebar/ai.mjs
@@ -5,8 +5,9 @@ export default [
       { text: 'AI 首页', link: '/ai/' },
       { text: 'AI名词汇总', link: '/ai/ai-names-info' },
       { text: '大模型微调介绍&理论', link: '/ai/llm-fine-tuning-intro' },
-      { text: 'openclaw使用体验', link: '/ai/openclaw-after-user' },
-      { text: 'openclaw使用体验(二)', link: '/ai/openclaw-after-use-2' }
+      { text: 'openClaw使用体验', link: '/ai/openclaw-after-user' },
+      { text: 'openClaw使用体验(二)', link: '/ai/openclaw-after-use-2' },
+      { text: 'openClaw项目分析-概述', link: '/ai/openclaw-analysis' }
     ]
   }
 ];
