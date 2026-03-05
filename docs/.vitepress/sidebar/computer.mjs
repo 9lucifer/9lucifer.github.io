5012a7ad59e9fe6074cf6d41cd7c8be5fb034006
9lucifer <3616266449@qq.com>
2026-03-05 22:36:29 +0800

    feat:add 死锁

--- docs/.vitepress/sidebar/computer.mjs
+++ docs/.vitepress/sidebar/computer.mjs
@@ -3,6 +3,7 @@ export default [
     text: '计算机基础',
     items: [
       { text: '计算机网络重点知识', link: '/computer/network/net-info' },
+      { text: '死锁知识梳理', link: '/computer/os/os_dead_lock' },
     ]
   }
 ];
