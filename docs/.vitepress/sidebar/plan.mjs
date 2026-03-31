7c6825217e7b52defd3fd47e474739285f5eb729
9lucifer <3616266449@qq.com>
2026-03-31 23:03:49 +0800

    add 个人规划2026 q

--- docs/.vitepress/sidebar/plan.mjs
+++ docs/.vitepress/sidebar/plan.mjs
@@ -8,6 +8,7 @@ export default [
         collapsed: false,
         items: [
           { text: '2026', link: '/plan/2026/index' },
+          { text: '2026-03-06 规划', link: '/plan/2026/2026_03_06' },
         ]
       }
     ]
